25f121814a85c75c82c471eb6f3e1de730e14abb
puru7791 <puru7791@gmail.com>
2020-06-03 20:37:45 +0530

    update tf files

--- bahmni_installation.sh
+++ bahmni_installation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Prerequisite for the fresh installation of Bahmni
-yum install -y https://kojipkgs.fedoraproject.org//packages/zlib/1.2.11/19.fc30/x86_64/zlib-1.2.11-19.fc30.x86_64.rpm -y
+sudo yum install -y https://kojipkgs.fedoraproject.org//packages/zlib/1.2.11/19.fc30/x86_64/zlib-1.2.11-19.fc30.x86_64.rpm -y
 sleep 10
-yum install https://dl.bintray.com/bahmni/rpm/rpms/bahmni-installer-0.92-147.noarch.rpm -y 
+sudo yum install https://dl.bintray.com/bahmni/rpm/rpms/bahmni-installer-0.92-147.noarch.rpm -y 
 sleep 10
 curl -L https://tinyurl.com/yyoj98df >> /etc/bahmni-installer/setup.yml
 sleep 5
